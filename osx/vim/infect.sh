5d404420c5bf350f8c3da8067d3abbd5ad80fa33
Jeffrey D. McGovern <rectangleboy@gmail.com>
2017-04-12 14:18:38 -0700

    finally have a way of gettings colors to automatically work

--- osx/vim/infect.sh
+++ osx/vim/infect.sh
@@ -2,7 +2,7 @@
 
 DEIN_INSTALLER="https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh"
 DEIN_INSTALLER_NAME="dein-installer.sh"
-DEIN_INSTALL_LOC="${HOME}/.vim/dein/repos/github.com/Shougo/dein.vim"
+DEIN_INSTALL_LOC="${HOME}/.vim/bundles"
 
 echo "INSTALLING DEIN USING" ${DEIN_INSTALLER}
 curl ${DEIN_INSTALLER} > ${DEIN_INSTALLER_NAME} 
@@ -10,5 +10,13 @@ curl ${DEIN_INSTALLER} > ${DEIN_INSTALLER_NAME}
 echo "RUNNING" ${DEIN_INSTALLER_NAME}
 echo "INSTALL DIRECTORY:" ${DEIN_INSTALL_LOC}
 sh ./${DEIN_INSTALLER_NAME} ${DEIN_INSTALL_LOC}
+
+CUR_DIR=$(pwd)
+
+echo Changing from ${CUR_DIR} to ${HOME}
+cd ${HOME}
+pwd
+ln -sv "${HOME}/.dot/osx/vim/vimrc" .vimrc
+cd ${CUR_DIR}
 
 exit 0
