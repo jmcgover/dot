12c11e5e102f74c6d9861971b446f7beaef543f9
Jeffrey D. McGovern <jmcgovern@apple.com>
2017-07-17 11:44:37 -0700

    getting rid of dein

--- osx/vim/infect.sh
+++ osx/vim/infect.sh
@@ -1,22 +1,13 @@
 #! /bin/env sh
 
-DEIN_INSTALLER="https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh"
-DEIN_INSTALLER_NAME="dein-installer.sh"
-DEIN_INSTALL_LOC="${HOME}/.vim/bundles"
-
-echo "INSTALLING DEIN USING" ${DEIN_INSTALLER}
-curl ${DEIN_INSTALLER} > ${DEIN_INSTALLER_NAME} 
-
-echo "RUNNING" ${DEIN_INSTALLER_NAME}
-echo "INSTALL DIRECTORY:" ${DEIN_INSTALL_LOC}
-sh ./${DEIN_INSTALLER_NAME} ${DEIN_INSTALL_LOC}
+VUNDLE_REPO=
 
 CUR_DIR=$(pwd)
 
 echo Changing from ${CUR_DIR} to ${HOME}
 cd ${HOME}
 pwd
-ln -sv "${HOME}/.dot/osx/vim/vimrc" .vimrc
+ln -sv "${CUR_DIR}/vimrc" .vimrc
 cd ${CUR_DIR}
 
 exit 0
