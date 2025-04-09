010dc50b5d1b865231d8e764d7a6fd7e85df1295
Jeffrey McGovern <jeff@jdmcg.org>
2025-04-09 01:18:33 -0700

    sync over some cows from the homebrew install

---
+++ cows/infect.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -o noclobber nounset errexit
+
+COWDIR="cows"
+
+function check_targets() {
+  echo 'Checking targets...'
+  target_filename="${HOME}/.${COWDIR}"
+  if [ -f "${target_filename}" ]; then
+    echo "Target file already exists: ${target_filename}"
+    exit 22
+  fi
+  if [ -d "${target_filename}" ]; then
+    echo "Target directory already exists: ${target_filename}"
+    exit 22
+  fi
+  echo "Does not exist: ${target_filename}"
+  echo 'Checking targets DONE'
+}
+
+
+function check_sources() {
+  echo 'Checking sources...'
+  basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  if [ ! -d "${basedir}" ]; then
+    echo "Source ${basedir} directory does not exist: ${source_filename}"
+    exit 22
+  fi
+  echo 'Checking sources DONE'
+}
+
+function link_dotfiles() {
+  echo 'Linking dotfiles...'
+  basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  ln -v -w -s "${basedir}" "${HOME}/.${COWDIR}"
+  echo 'Linking dotfiles DONE'
+}
+
+function main() {
+  echo 'Infecting...'
+  check_targets
+  check_sources
+  link_dotfiles
+  echo 'Infecting DONE'
+}
+
+main "$@"
