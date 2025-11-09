bc0357ada228fefa44b053ee7cd7ee079a47bab3
Jeffrey McGovern <jeff@jdmcg.org>
2025-11-08 18:56:38 -0800

    add yay infect and rename pkglist

---
+++ cachyos/yay/infect.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o noclobber nounset errexit
+
+PKGLISTS=("aur.txt")
+
+function check_sources() {
+  echo 'Checking sources...'
+  local basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  for base in "${PKGLISTS[@]}"; do
+    local source_filename="${basedir}/${base}"
+    if [ ! -f "${source_filename}" ]; then
+      echo "Source file does not exist: ${source_filename}"
+      exit 22
+    fi
+    echo "Exists: ${source_filename}"
+  done
+  echo 'Checking sources DONE'
+}
+
+function install_packages() {
+  echo 'Installing packages...'
+  local basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  local pkglists_absolute=()
+  for pkglist in "${PKGLISTS[@]}"; do
+    pkglists_absolute+=("${basedir}/${pkglist}")
+  done
+  set -o xtrace
+  cat "${pkglists_absolute[@]}" | sort | uniq
+  cat "${pkglists_absolute[@]}" | yay --needed --sync --refresh --refresh --sysupgrade -
+  set +o xtrace
+  echo 'Installing packages DONE'
+}
+
+function main() {
+  echo 'Infecting...'
+  check_sources
+  install_packages
+  echo 'Infecting DONE'
+}
+
+main "$@"
+
