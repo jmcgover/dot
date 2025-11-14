402201fd8297627549ad8e9565fc9c978dfd4a30
Jeffrey McGovern <jeff@jdmcg.org>
2025-11-13 23:37:40 -0800

    add rustup completions

---
+++ macos/ticker/infect.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o noclobber nounset errexit
+
+TICKERFOLDER="ticker"
+
+function check_targets() {
+  echo 'Checking targets...'
+  global_config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}"
+  if [ ! -d "${global_config_dir}" ]; then
+    echo "Global config folder does not exist: ${global_config_dir}"
+    exit 22
+  fi
+  echo 'Checking targets DONE'
+}
+
+function check_sources() {
+  echo 'Checking sources...'
+  basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  if [ ! -d "${basedir}" ]; then
+    echo "Source directory somehow does not exist: ${basedir}"
+    exit 22
+  fi
+  echo 'Checking sources DONE'
+}
+
+function link_dotfiles() {
+  echo 'Linking dotfiles...'
+  basedir="$(dirname $(realpath ${BASH_SOURCE:-$0}))"
+  global_config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}"
+  ln -v -w -s "${basedir}" "${global_config_dir}/${TICKERFOLDER}"
+  echo 'Linking dotfiles DONE'
+}
+
+function main() {
+  echo 'Infecting...'
+  check_targets
+  check_sources
+  link_dotfiles
+  echo 'Infecting DONE'
+}
+
+main "$@"
