add1b59d49b6a8782e6857a183692089a65b0766
Jeffrey McGovern <jeff@jdmcg.org>
2026-08-05 16:37:57 -0700

    format comment to width

--- macos/nvim/lua/plugins/init.lua
+++ macos/nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 print(("%s: Hello!"):format(debug.getinfo(1,'S').source))
--- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be automatically merged in the main plugin spec
+-- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be automatically
+-- merged in the main plugin spec
 return {
 	"folke/lazydev.nvim",  -- properly configures LuaLS for editing your Neovim config by lazily updating your workspace libraries
 	"folke/which-key.nvim", -- shows available keybindings in a popup as you type
