81095fe0b7c8a0d91b55f3952e95c4ed81ca6e06
Jeffrey McGovern <jeff@jdmcg.org>
2026-08-07 14:51:14 -0700

    python LSP appears to work -- need to get a code actions preview

--- macos/nvim/lua/rectangleboy/lsp.lua
+++ macos/nvim/lua/rectangleboy/lsp.lua
@@ -2,12 +2,12 @@
 -- COMMANDS: jump
 --                          *CTRL-O*
 -- CTRL-O           Go to [count] Older cursor position in jump list
--- 
+--
 -- <Tab>        or                  *CTRL-I* *<Tab>*
 -- CTRL-I           Go to [count] newer cursor position in jump list
 -- COMMANDS: lsp
 -- GLOBAL DEFAULTS                       *gra* *gri* *grn* *grr* *grt* *grx* *i_CTRL-S*
--- 
+--
 -- These GLOBAL keymaps are created unconditionally when Nvim starts:
 -- - "gra" (Normal and Visual mode) is mapped to |vim.lsp.buf.code_action()|
 -- - "gri" is mapped to |vim.lsp.buf.implementation()|
@@ -27,19 +27,20 @@
 --     )
 --
 -- FORMAT
-vim.keymap.set('n', '<C-b>', '<Plug>(nvim.lsp.ctrl-s)', {desc = "Displays signature information about the symbol under the cursor in a floating window."})
+vim.keymap.set('n', '<C-b>', '<Plug>(nvim.lsp.ctrl-s)',
+    { desc = "Displays signature information about the symbol under the cursor in a floating window." })
 
-vim.keymap.set('n', '<C-k>', function() 
+vim.keymap.set('n', '<C-k>', function()
     vim.lsp.buf.format({ async = false })
-end, {desc = "Formats a buffer using the attached (and optionally filtered) language server clients."})
+end, { desc = "Formats a buffer using the attached (and optionally filtered) language server clients." })
 
 -- LANGUAGE: lua
 -- Install: Global
 -- SERVER: luals
 vim.lsp.config('luals', {
-  cmd = {'lua-language-server'},
-  filetypes = {'lua'},
-  root_markers = {'.luarc.json', '.luarc.jsonc'},
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { '.luarc.json', '.luarc.jsonc' },
 })
 
 -- LANGUAGE: python
@@ -48,7 +49,7 @@ vim.lsp.config('luals', {
 vim.lsp.config('ruff', {
     cmd = { 'ruff', 'server' },
     filetypes = { 'python' },
-    root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', },
+    root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
     init_options = {
         settings = {
             logLevel = 'debug',
@@ -56,21 +57,34 @@ vim.lsp.config('ruff', {
     }
 })
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then
-      return
-    end
-    if client.name == 'ruff' then
-      -- Disable hover in favor of Pyright
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-  desc = 'LSP: Disable hover capability from Ruff',
+    group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+            return
+        end
+        if client.name == 'ruff' then
+            -- Disable hover in favor of the other LSPs
+            client.server_capabilities.hoverProvider = false
+        end
+    end,
+    desc = 'LSP: Disable hover capability from Ruff',
 })
 -- SERVER: pylsp
+vim.lsp.config('pylsp', {
+    cmd = { 'pylsp' },
+    filetypes = { 'python' },
+    root_markers = {
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'Pipfile',
+        '.git',
+    },
+})
 
 -- ENABLE
 vim.lsp.enable('luals')
 vim.lsp.enable('ruff')
+vim.lsp.enable('pylsp')
