1cb1821d774c03f7b6657a3f1197930b1eab1eb9
Jeffrey McGovern <jeff@jdmcg.org>
2026-08-07 10:56:41 -0700

    retab, improve comments

--- macos/nvim/lua/plugins/telescope.lua
+++ macos/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,26 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim', -- highly extendable fuzzy finder over lists
-		version = '*',
-		enabled = true,  -- When false, or if the function returns false, then this plugin will not be included in the spec
-		dependencies = {  -- list of plugin names or plugin specs that should be loaded when the plugin loads
-			'nvim-lua/plenary.nvim', -- Lua module for asynchronous programming using coroutines
-			-- optional but recommended
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-		},
-		config = function()  -- executed when the plugin loads
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>ps', function ()
-				builtin.grep_string({ search = vim.fn.input("grep > ")});
-			end, { desc = 'Telescope grep string' })
-		end,
-	}
+    {
+        'nvim-telescope/telescope.nvim', -- highly extendable fuzzy finder over lists
+        version = '*',
+        enabled = true,  -- When false, or if the function returns false, then this plugin will not be included in the spec
+        dependencies = {  -- list of plugin names or plugin specs that should be loaded when the plugin loads
+            { -- Lua module for asynchronous programming using coroutines
+                'nvim-lua/plenary.nvim',
+            },
+            { --  C port of fzf only covering the algorithm and implements
+              --  few functions to support calculating the score
+              --  (optional but recommended)
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+            },
+        },
+        config = function()  -- executed when the plugin loads
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>ps', function ()
+                builtin.grep_string({ search = vim.fn.input("grep > ")});
+            end, { desc = 'Telescope grep string' })
+        end,
+    }
 }
